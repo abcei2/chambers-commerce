d14427fe87240f230b548c934064f4222f6a03f0
abcei2 <santiago.salgado.duque@gmail.com>
2022-12-24 18:05:19 -0500

    correction

--- components/dashboard/MainChart.tsx
+++ components/dashboard/MainChart.tsx
@@ -3,7 +3,6 @@ import { Doughnut, Radar, Pie, Bar } from 'react-chartjs-2';
 import "chart.js/auto";
 import React, { useCallback, useState } from 'react';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
-import generateColors from '../../utils/colorgen';
 
 
 
