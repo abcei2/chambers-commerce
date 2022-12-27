a33db8656237fc18ac086f55e24aeaa960602cc1
abcei2 <santiago.salgado.duque@gmail.com>
2022-12-27 07:27:47 -0500

    closing menu when user click outside div

--- components/heatmap/Filter.tsx
+++ components/heatmap/Filter.tsx
@@ -2,14 +2,14 @@ import { useContext, useEffect, useState } from "react";
 import Select from 'react-select'
 import { filterFields } from "../../constants";
 import { HeatMapContext } from "../../context/HeatMapContext";
+import useComponentVisible from "../../hooks/useComponentVisible";
 
 const Filter = () => {
 
-    const [openMenu, setOpenMenu] = useState(false)
     const [selectsOption, setSelectOption] = useState<any>()
     const [filterOptions, setFilterOptions] = useState<any>({})
     const { updateData } = useContext(HeatMapContext)
-
+    const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(true);
 
     useEffect(() => {
         Object.keys(filterFields).forEach(
@@ -47,11 +47,11 @@ const Filter = () => {
     }
 
     return (
-        <div className="flex justify-end">
+        <div className="flex justify-end" ref={ref}>
 
             
             {
-                openMenu && <div className='w-80   max-h-[90%] pointer-events-none   overflow-visible '>
+                isComponentVisible && <div className='w-80   max-h-[90%] pointer-events-none   overflow-visible '>
 
                     <div className='bg-[var(--secondary-color)] rounded-[20px] flex justify-center flex-col p-5 gap-5 max-h-[90%] overflow-visible pointer-events-auto'>
                     {
@@ -89,7 +89,7 @@ const Filter = () => {
                 </div>
             </div>
             }
-            <div className="bg-[var(--secondary-color)] w-fit h-fit rounded-lg p-2 pointer-events-auto" onClick={() => setOpenMenu(!openMenu)}>
+            <div className="bg-[var(--secondary-color)] w-fit h-fit rounded-lg p-2 pointer-events-auto" onClick={() => setIsComponentVisible(!isComponentVisible)}>
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
             </div>
         </div>
