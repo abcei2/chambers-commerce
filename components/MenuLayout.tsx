3c575ac353eb718441d146e72908f1d6653bf344
abcei2 <santiago.salgado.duque@gmail.com>
2022-12-29 01:34:35 -0500

    route to dashboard whe click on logo

--- components/MenuLayout.tsx
+++ components/MenuLayout.tsx
@@ -7,13 +7,13 @@ const MenuLayout = (props:{
     const [showNames, setShowNames] = useState(false)
     const router = useRouter()
     return <div className="">
-        <div className="bg-gray-200 ">
+        <button className=" " onClick={() => router.push("/")}>
             <img src="/logo.png" className="h-[120px]"/>
-        </div>
+        </button>
         <div className="py-5 md:pl-2">
-            <div className="flex flex-col bg-[var(--secondary-color)] absolute rounded-[20px]   justify-center   z-10 " onMouseEnter={() => setShowNames(true)}
+            <div className="flex flex-col bg-[var(--secondary-color)] absolute rounded-[20px]   justify-center   z-10 " 
                 onMouseLeave={() => setShowNames(false)}>
-                <div className="layout-menu-item rounded-t-[20px]"  onClick={()=> setShowNames(!showNames)}>
+                <div className="layout-menu-item rounded-t-[20px]"  onMouseEnter={() => setShowNames(true)}>
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg> 
                     {showNames?"Menú":""}
                 </div>
